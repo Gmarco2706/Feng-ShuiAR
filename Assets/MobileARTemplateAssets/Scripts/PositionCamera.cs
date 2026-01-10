65047c3ab46d41a3bf2f3c057b047f2aec262fff
Pietro <la_tua_email@esempio.com>
2026-01-10 12:20:23 +0100

    modifica logica menu con aggiunta di un elenco di etichette per la selezione,modifica struttura del menu e dei bottoni,aggiunta prefab delle etichette per lo spawner degli oggetti tramite tap dell'utente.Modifica schermata di avvio dell'applicazione adattata al contesto.

---
+++ Assets/MobileARTemplateAssets/Scripts/PositionCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.MobileARTemplateAssets.Scripts
+{
+    public class PositionCamera : MonoBehaviour
+    {
+
+        // Use this for initialization
+
+        private Camera mainCamera;
+        void Start()
+        {
+           mainCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+                         mainCamera.transform.rotation * Vector3.up);
+
+        }
+    }
+}
