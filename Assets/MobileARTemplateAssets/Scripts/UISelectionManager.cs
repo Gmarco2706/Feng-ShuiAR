09bfd37f58749885526b1ebd3b7d3e5c0d30745e
Pietro <la_tua_email@esempio.com>
2026-01-16 17:27:00 +0100

    modifica menù etichette oggetti con aggiunta separazione per categoria zona mappa,aggiunta di altre etichette identificative,ripristino indicizzazione,creazione prefab etichette e aggiunta all object spawner.Aggiunta script per gestione selection box riquadro di ogni etichetta selezionata,modifica layout contenitore bottoni menù con ripristino a quello verticale.

---
+++ Assets/MobileARTemplateAssets/Scripts/UISelectionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.MobileARTemplateAssets.Scripts
+{
+    public class UISelectionManager : MonoBehaviour
+    {
+
+        public List<GameObject> allSelectionBoxes;
+
+        // Questa funzione la colleghi al bottone
+        public void SelectOnly(GameObject activeBox)
+        {
+            // Spegniamo tutti gli altri riquadri di selezione
+            foreach (GameObject box in allSelectionBoxes)
+            {
+                if (box != null) box.SetActive(false);
+            }
+
+            // Riaccendiamo solo quello attivo
+            if (activeBox != null) activeBox.SetActive(true);
+        }
+    }
+}
